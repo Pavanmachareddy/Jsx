8ac9e10e6e6cb04ec6d332f672e78df16b92a65f
Pavanmachareddy <pavanrmacha@gmail.com>
2022-08-02 00:28:55 +0530

    Added refs with collageName

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge,uCollage) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge, collage: uCollage, id: Math.random().toString() },
       ];
     });
   };
 
   return (
-    <React.Fragment>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </React.Fragment>
+    </Fragment>
   );
 }
 
